00476fc451964e271545afe05aade2d0c19a117b
Daramola femi <dfmopeoluwa@gmail.com>
2022-03-10 14:43:51 +0100

    Search done

--- components/Search.js
+++ components/Search.js
@@ -4,9 +4,13 @@ function Search({ placeholder, icon }) {
     <div className="search">
       <form
         action=""
-        className="search__Form p-2 h-[46px] flex justify-center align-center bg-white  opacity-60 text-black rounded-[42px] outline-0 "
+        className="search__Form p-2 h-[46px] flex justify-center align-center bg-white  opacity-60 text-black rounded-[42px]"
       >
-        <input type="text" placeholder={placeholder} className=" mr-2 px-2" />
+        <input
+          type="text"
+          placeholder={placeholder}
+          className=" mr-2 px-2 outline-0 border-none w-60"
+        />
         <Image src={icon} />
       </form>
     </div>
